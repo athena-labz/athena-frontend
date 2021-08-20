a64f2338e7b88feb1c21c8d9dad35dd6a4107eac
samuelssan <42661697+SamuelSSan28@users.noreply.github.com>
2021-08-20 00:10:26 -0300

    add v1 of service cards
    and do a request to get the services

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
-import Header  from '../components/Header'
-import Footer  from '../components/Footer'
 import styles from '../styles/Home.module.css'
 import HomeSection from '../components/HomeSection'
 import FeaturesSection from '../components/FeatureSection'
+import { api } from '../services/api'
+import ServiceCard from '../components/ServiceCard'
 
-const Home: NextPage = () => {
+
+type Service= {
+  type: string;
+  publisher: string;
+  title: string;
+  description: string;
+  trust: number;
+  price?: number;
+  deadline?: number;
+  badge_color:string;
+}
+
+
+type Homeprops ={
+  services: Service[]
+}
+
+
+const Home = ({services}:Homeprops) => {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -19,11 +38,47 @@ const Home: NextPage = () => {
         <HomeSection />
 
         <FeaturesSection />
+
+        <h1>-----Services: --------</h1>
+        <ul>
+
+          {services.map((service)=>(
+            <li key={service.title}>
+             
+               <ServiceCard
+                type={service.type}
+                badge_color={service.badge_color}
+                publisher={service.publisher}
+                title={service.title}
+                description={service.description}
+                trust={service.trust}
+                price={service.price}
+                deadline={service.deadline}
+               />
+
+            </li>
+          ))}
+
+        </ul>
+      
         
        </section >
  
     </div>
   )
+}
+
+export const getStaticProps:GetStaticProps = async () => {
+  
+  const {data} =  await api("services",{});
+
+  return {
+    props:{
+      services:data.services
+      
+    },
+   
+  }
 }
 
 export default Home
