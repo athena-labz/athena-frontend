b1ae391d4599475641c8c9cfba67d52c80cadd7c
samuelssan <85943933+samuelssan28ic@users.noreply.github.com>
2021-08-20 00:07:41 -0300

    restrict routes with footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,21 @@ import { ChakraProvider } from "@chakra-ui/react"
 import { theme } from '../theme'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const pathnames_not_show_footer = ['/login','/forgot-password','/reset-password']
+  
   return (
     <ChakraProvider theme={theme}> 
       <main style={{flex:1}}>
           <Header />
 
           <Component {...pageProps} />
+          
+          {pathnames_not_show_footer.indexOf(router.pathname) === -1 &&  <Footer /> }
          
-          <Footer />
           
        </main>
     </ChakraProvider>
