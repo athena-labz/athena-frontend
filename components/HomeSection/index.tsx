ef5ff2f766dab28351fa4f644f9f47a810ef481f
samuelssan <42661697+SamuelSSan28@users.noreply.github.com>
2021-09-06 23:55:24 -0300

    refactor home section

--- components/HomeSection/index.tsx
+++ components/HomeSection/index.tsx
@@ -1,61 +1,149 @@
-import type { NextPage } from 'next'
 import {
-    Flex,
-    Container,
-    Heading,
-    Stack,
-    Text,
-    Button,
-    Icon,
-    IconProps,
-  } from '@chakra-ui/react';
-import Image from 'next/image';
+  chakra,
+  Box,
+  useColorModeValue,
+  Icon,
+  Image,
+  HStack,
+  Stack,
+} from "@chakra-ui/react";
+
   
 export default function HomeSection() {
-    return (
-      <Container maxW={'5xl'} >
-         <Stack
-        textAlign={'center'}
-        align={'center'}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 20, md: 28 }}>
-        <Heading
-          fontWeight={600}
-          fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-          lineHeight={'110%'}>
-          Provide reliable services {' '}
-          <Text as={'span'} color={'blue.400'}>
-            made easy
-          </Text>
-        </Heading>
-        <Text color={'gray.500'} maxW={'3xl'}>
-        Building trust among peers through blockchain technology
-        </Text>
-        <Stack spacing={6} direction={'row'}>
-          <Button
-            as={'a'} 
-            rounded={'full'}
-            px={6}
-            colorScheme={'blue'}
-            bg={'blue.400'}
-             href={'https://github.com/mateusap1/DigiServic'} 
-            _hover={{ bg: 'blue.500' }}>
-            White Paper
-          </Button>
-          <Button 
-            as={'a'} 
-            rounded={'full'} px={6}
-                href={'/solution'} >
-            Learn more
-          </Button>
-        </Stack>
-
-        <Image src="/Digiservices.png" width="540rem" height="350rem" />
-      </Stack>
-
-        
-
-      </Container>
-    );
-  }
-  
+  const bg = useColorModeValue("white", "gray.800");
+  return (
+    <Box pos="relative" overflow="hidden" bg={bg} mt={10}>
+      <Box maxW="7xl" mx="auto">
+        <Box
+          pos="relative"
+          pb={{ base: 8, sm: 16, md: 20, lg: 28, xl: 32 }}
+          maxW={{ lg: "2xl" }}
+          w={{ lg: "full" }}
+          zIndex={1}
+          bg={bg}
+          border="solid 1px transparent"
+        >
+          <Icon
+            display={{ base: "none", lg: "block" }}
+            position="absolute"
+            right={0}
+            top={0}
+            bottom={0}
+            h="full"
+            w={48}
+            color={bg}
+            transform="translateX(50%)"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </Icon>
+          <Box
+            mx="auto"
+            maxW={{ base: "7xl" }}
+            px={{ base: 4, sm: 6, lg: 8 }}
+            mt={{ base: 10, sm: 12, md: 16, lg: 20, xl: 28 }}
+          >
+            <Box
+              w="full"
+              textAlign={{ sm: "center", lg: "left" }}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <chakra.h1
+                fontSize={{ base: "4xl", sm: "5xl", md: "6xl" }}
+                letterSpacing="tight"
+                lineHeight="short"
+                fontWeight="extrabold"
+                color={useColorModeValue("gray.900", "white")}
+              >
+                <chakra.span display={{ base: "block", xl: "inline" }}>
+                 Provide reliable services{" "}
+                </chakra.span>
+                <chakra.span
+                  display={{ base: "block", xl: "inline" }}
+                  color={useColorModeValue("blue.600", "blue.400")}
+                >
+                 made easy
+                </chakra.span>
+              </chakra.h1>
+              <chakra.p
+                mt={{ base: 3, sm: 5, md: 5 }}
+                fontSize={{ sm: "lg", md: "xl" }}
+                maxW={{ sm: "xl" }}
+                mx={{ sm: "auto", lg: 0 }}
+                color="gray.500"
+              >
+                Building trust among peers through blockchain technology.
+              </chakra.p>
+              <Box
+                mt={{ base: 5, sm: 8 }}
+                display={{ sm: "flex" }}
+                justifyContent={{ sm: "center", lg: "start" }}
+                fontWeight="bold"
+              >
+                <Box rounded="full" shadow="md">
+                  <chakra.a
+                    w="full"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    border="solid 1px transparent"
+                    fontSize={{ base: "md", md: "lg" }}
+                    rounded="lg"
+                    color="white"
+                    bg={'blue.400'}
+                   _hover={{ bg: 'blue.500' }}
+                    px={{ base: 8, md: 10 }}
+                    py={{ base: 3, md: 4 }}
+                    cursor="pointer"
+                  >
+                    White Paper
+                  </chakra.a>
+                </Box>
+                <Box mt={[3, 0]} ml={[null, 3]}>
+                  <chakra.a
+                    w="full"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    px={{ base: 8, md: 10 }}
+                    py={{ base: 3, md: 4 }}
+                    border="solid 1px transparent"
+                    fontSize={{ base: "md", md: "lg" }}
+                    rounded="lg"
+                    color="black.200"
+                    bg="gray.300"
+                    _hover={{ bg: "gray.200" }}
+                    cursor="pointer"
+                  >
+                   Learn More
+                  </chakra.a>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        position={{ lg: "absolute" }}
+        top={{ lg: 0 }}
+        bottom={{ lg: 0 }}
+        right={{ lg: 0 }}
+        w={{ lg: "50%" }}
+        border='solid 1px transparent'
+      >
+        <Image
+          h={[56, 72, 96, "full"]}
+          w="full"
+          fit="cover"
+          src="/Digiservices.png"
+          alt=""
+          loading="lazy"
+        />
+      </Box>
+    </Box>
+  );
+};
