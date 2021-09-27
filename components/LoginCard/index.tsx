b0d5c81b48b3f0c98e5b5b221552a9472071e4f2
Samuel <42661697+SamuelSSan28@users.noreply.github.com>
2021-09-26 23:16:21 -0300

    add user/judge selector

--- components/LoginCard/index.tsx
+++ components/LoginCard/index.tsx
@@ -1,31 +1,47 @@
+import { useState } from 'react';
+
 import {
     Box,
     FormControl,
     FormLabel,
     Input,
+    RadioGroup ,
+    Radio ,
     Checkbox,
     Stack,
     Link,
     Button,
     Heading,
+    Center,
     Text,
     useColorModeValue,
   } from '@chakra-ui/react';
   
   export default function LoginCard() {
+    const [value, setValue] = useState("1")
+
     return (
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={6} px={3}>
+        <Stack spacing={1} mx={'auto'} maxW={'md'} py={4} px={3}>
           <Stack align={'center'}>
-            <Heading fontSize={'5xl'}>Sign in to your account</Heading>
-            <Text fontSize={'lg'} color={'gray.600'}>
-              to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
-            </Text>
-          </Stack>
+            <Heading fontSize={'5xl'}>Sign in to your account</Heading>           
+          </Stack> 
+          <Center>
+          <RadioGroup defaultValue="2" onChange={setValue} value={value}>
+              <Stack spacing={5} direction="row" >             
+                  <Radio size="md" name="1" value="1" colorScheme="blue" >
+                    User
+                  </Radio>
+                  <Radio size="md" name="2" value="2" colorScheme="blue">
+                    Judge
+                  </Radio>
+              </Stack>
+            </RadioGroup></Center>
           <Box
             rounded={'lg'}
             bg={useColorModeValue('white', 'gray.700')}
             boxShadow={'lg'}
             p={8}>
+            
             <Stack spacing={4}>
               <FormControl id="email">
                 <FormLabel>Email address</FormLabel>
