f722b2cf26847ebdbe1cb3259cc46ecf87229cd6
Mateus Santos <santos.mateus@protonmail.com>
2022-03-18 13:29:59 -0300

    change wallet selection to modal

--- components/RegisterCard/index.tsx
+++ components/RegisterCard/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import Router from 'next/router'
+import { useState } from "react";
+import Router from "next/router";
 import {
   Box,
   FormControl,
@@ -15,8 +15,13 @@ import {
   Center,
   Text,
   useColorModeValue,
-} from '@chakra-ui/react';
-import { useUser } from '../../contexts/UserContext';
+} from "@chakra-ui/react";
+import WalletSelector from "../WalletSelector";
+
+import { useUser } from "../../contexts/UserContext";
+import { useWallet } from "../../contexts/WalletContext";
+
+import { bech32addr } from "../../wallet/utils";
 
 type Role = "Proposer" | "Contributor" | "Mediator";
 
@@ -25,16 +30,19 @@ type UserData = {
   name: string;
   email: string;
   password: string;
-}
+};
 
 export default function RegisterCard() {
+  const { connect } = useWallet();
+
   const [userData, setUser] = useState<UserData>({
     email: "",
     password: "",
     name: "",
-    role: "Contributor"
-  })
-  const { saveInfo } = useUser();
+    role: "Contributor",
+  });
+  const [openWalletSelector, setOpenWalletSelector] = useState(false);
+  const { register } = useUser();
 
   function setName(name: string) {
     setUser({ ...userData, name: name });
@@ -52,67 +60,128 @@ export default function RegisterCard() {
     setUser({ ...userData, role: role });
   }
 
-  return (
-    <Stack spacing={1} mx={'auto'} maxW={'md'} py={4} px={3}>
-      <Stack align={'center'}>
-        <Heading fontSize={'5xl'}>Create your account</Heading>
-      </Stack>
-      <Center>
-        <RadioGroup defaultValue="2" onChange={setRole} value={userData.role}>
-          <Stack spacing={5} direction="row" >
-            <Radio size="md" name="Proposer" value="Proposer" colorScheme="blue" >
-              Proposer
-            </Radio>
-            <Radio size="md" name="Contributor" value="Contributor" colorScheme="blue">
-              Contributor
-            </Radio>
-            <Radio size="md" name="Mediator" value="Mediator" colorScheme="blue" >
-              Mediator
-            </Radio>
-          </Stack>
-        </RadioGroup></Center>
-      <Box
-        rounded={'lg'}
-        bg={useColorModeValue('white', 'gray.700')}
-        boxShadow={'lg'}
-        p={8}>
+  function registrationFailed() {
+    console.log("Registration failed")
+  }
 
-        <Stack spacing={4}>
-          <FormControl id="email">
-            <FormLabel>Name</FormLabel>
-            <Input value={userData.name} onChange={evt => setName(evt.target.value)} />
-          </FormControl>
-          <FormControl id="email">
-            <FormLabel>Email address</FormLabel>
-            <Input type="email" value={userData.email} onChange={evt => setEmail(evt.target.value)} />
-          </FormControl>
-          <FormControl id="password">
-            <FormLabel>Password</FormLabel>
-            <Input type="password" value={userData.password} onChange={evt => setPassword(evt.target.value)} />
-          </FormControl>
-          <Stack spacing={10}>
-            <Stack
-              direction={{ base: 'column', sm: 'row' }}
-              align={'start'}
-              justify={'space-between'}>
-              <Checkbox>Remember me</Checkbox>
-              <Link href="/forgot-password" color={'blue.400'}>Forgot password?</Link>
-            </Stack>
-            <Button
-              onClick={e => {
-                saveInfo(userData.role, userData.name, userData.email, userData.password);
-                Router.push('/select-wallet');
-              }}
-              bg={'blue.400'}
-              color={'white'}
-              _hover={{
-                bg: 'blue.500',
-              }}>
-              Sign up
-            </Button>
-          </Stack>
+  return (
+    <>
+      <Stack spacing={1} mx={"auto"} maxW={"md"} py={4} px={3}>
+        <Stack align={"center"}>
+          <Heading fontSize={"5xl"}>Create your account</Heading>
         </Stack>
-      </Box>
-    </Stack>
+        <Center>
+          <RadioGroup defaultValue="2" onChange={setRole} value={userData.role}>
+            <Stack spacing={5} direction="row">
+              <Radio
+                size="md"
+                name="Proposer"
+                value="Proposer"
+                colorScheme="blue"
+              >
+                Proposer
+              </Radio>
+              <Radio
+                size="md"
+                name="Contributor"
+                value="Contributor"
+                colorScheme="blue"
+              >
+                Contributor
+              </Radio>
+              <Radio
+                size="md"
+                name="Mediator"
+                value="Mediator"
+                colorScheme="blue"
+              >
+                Mediator
+              </Radio>
+            </Stack>
+          </RadioGroup>
+        </Center>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Name</FormLabel>
+              <Input
+                value={userData.name}
+                onChange={(evt) => setName(evt.target.value)}
+              />
+            </FormControl>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input
+                type="email"
+                value={userData.email}
+                onChange={(evt) => setEmail(evt.target.value)}
+              />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input
+                type="password"
+                value={userData.password}
+                onChange={(evt) => setPassword(evt.target.value)}
+              />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              >
+                <Checkbox>Remember me</Checkbox>
+                <Link href="/forgot-password" color={"blue.400"}>
+                  Forgot password?
+                </Link>
+              </Stack>
+              <Button
+                onClick={(e) => {
+                  if (
+                    userData.email.trim() !== "" &&
+                    userData.name.trim() !== "" &&
+                    userData.password.trim() !== ""
+                  ) {
+                    setOpenWalletSelector(true);
+                  }
+                }}
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                Sign up
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+      <WalletSelector
+        isOpen={openWalletSelector}
+        onSelect={async (wallet) => {
+          const result = await connect(wallet);
+
+          if (result.success === true && "api" in result) {
+            const addr = await bech32addr(result.api);
+            register(userData.role, userData.name, userData.email, addr, userData.password);
+          } else if (
+            result.success === false &&
+            "message" in result
+          ) {
+            console.log(result.message);
+          } else {
+            throw new Error("This message shouldn't be possible");
+          }
+        }}
+        onClose={() => setOpenWalletSelector(false)}
+      />
+    </>
   );
 }
--- contexts/UserContext.tsx
+++ contexts/UserContext.tsx
@@ -14,19 +14,10 @@ import jwt_decode from "jwt-decode";
 
 type Role = "Proposer" | "Contributor" | "Mediator";
 
-type UserInfoData = {
-  isLogged: boolean;
-  role: Role | null;
-  name: string | null;
-  email: string | null;
-  address: string | null;
-  password: string | null;
-};
-
 type UserContextData = {
-  user: UserInfoData;
-  saveInfo: (role: Role, name: string, email: string, password: string) => void;
-  register: (address: string) => void;
+  isSignedIn: () => boolean;
+  getUser: () => any;
+  register: (role: Role, name: string, email: string, address: string, password: string) => void;
   login: (email: string, password: string) => void;
   logout: () => void;
 };
@@ -38,48 +29,12 @@ type UserContextProviderProps = {
 const backend = axios.create({
   baseURL: "http://127.0.0.1:5000/",
   headers: { "Content-Type": "application/json" },
-  withCredentials: true
+  withCredentials: true,
 });
 
 export const UserContext_ = createContext({} as UserContextData);
 
 export function UserContextProvider({ children }: UserContextProviderProps) {
-  const [userInfo, setUserInfo] = useState<UserInfoData>({
-    isLogged: false,
-    role: null,
-    name: null,
-    email: null,
-    address: null,
-    password: null,
-  });
-
-  if (typeof window !== "undefined") {
-    useEffect(() => {
-      setUserInfo(
-        window.localStorage.getItem("userInfo") !== null
-          ? JSON.parse(sessionStorage.getItem("userInfo")!)
-          : userInfo
-      );
-    }, [window]);
-  }
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.sessionStorage.setItem("userInfo", JSON.stringify(userInfo));
-    }
-  }, [userInfo]);
-
-  function saveInfo(role: Role, name: string, email: string, password: string) {
-    if (name !== "" && password !== "")
-      setUserInfo({
-        ...userInfo,
-        role: role,
-        name: name,
-        email: email,
-        password: password,
-      });
-  }
-
   function getUser() {
     const token = localStorage.getItem("user");
     if (!token) {
@@ -89,31 +44,40 @@ export function UserContextProvider({ children }: UserContextProviderProps) {
     return jwt_decode(token);
   }
 
-  function register(address: string) {
-    const arg = {
-      name: userInfo.name,
-      email: userInfo.email,
-      role: userInfo.role,
-      address: address,
-      password: userInfo.password,
+  function isSignedIn() {
+    if (typeof window !== "undefined") {
+      return getUser() !== null;
     }
+
+    return false;
+  }
+
+  function register(
+    role: Role,
+    name: string,
+    email: string,
+    address: string,
+    password: string
+  ) {
+    const arg = {
+      name,
+      email,
+      role,
+      address,
+      password,
+    };
+
+    console.log(arg);
 
     backend
       .post("/register", arg)
       .then((res) => {
-        setUserInfo({ ...userInfo, address, isLogged: true });
-
         if (getUser() === null)
-          localStorage.setItem("user", res.data.access_token)
-        
-        console.log(getUser())
+          localStorage.setItem("user", res.data.access_token);
 
-        // clearInfo();
         Router.push("/");
       })
       .catch((error) => {
-        clearInfo();
-
         if (error.response && error.response.status === 400) {
           console.error(error.response.data.message);
         } else {
@@ -125,53 +89,33 @@ export function UserContextProvider({ children }: UserContextProviderProps) {
   function login(email: string, password: string) {
     backend
       .post("/login", { email: email, password: password })
-      .then(res => {
+      .then((res) => {
         if (getUser() === null)
-          localStorage.setItem("user", res.data.access_token)
-  
-        console.log(getUser())
+          localStorage.setItem("user", res.data.access_token);
 
         Router.push("/");
       })
-      .catch(error => {
-          if (error.response && error.response.status === 401) {
-            console.error(error.response.data.message);
-          } else {
-              throw error;
-          }
+      .catch((error) => {
+        if (error.response && error.response.status === 401) {
+          console.error(error.response.data.message);
+        } else {
+          throw error;
+        }
       });
   }
 
-  function clearInfo() {
-    setUserInfo({
-      isLogged: false,
-      role: null,
-      name: null,
-      email: null,
-      address: null,
-      password: null,
-    });
+  function logout() {
+    localStorage.removeItem("user");
   }
-
-  // function login(email: string, password: string) {
-  //   if (email === userInfo.email && password === userInfo.password) {
-  //     setUserInfo({ ...userInfo, isLogged: true });
-  //   }
-
-  //   // In the real application it should:
-  //   // * Back-end compare password hash to password in the DB
-  //   // * If it matches, return user info and let him log-in
-  //   // * Otherwise, warn user that he has the wrong password / email
-  // }
 
   return (
     <UserContext_.Provider
       value={{
-        user: userInfo,
-        saveInfo: saveInfo,
+        isSignedIn,
+        getUser,
         register: register,
         login: login,
-        logout: clearInfo,
+        logout: logout,
       }}
     >
       {children}
